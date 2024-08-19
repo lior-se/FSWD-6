516945f7bac081cc573aa9433f61b50a8a735d0d
S37M <setti@g.jct.ac.il>
2024-08-19 22:22:15 +0300

    gamepage

---
+++ my-react-app/src/components/GamePage.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
+function GamePage(){
+
+    return (
+        <h1>Game Info</h1>
+    );
+}
+
+export default GamePage;
